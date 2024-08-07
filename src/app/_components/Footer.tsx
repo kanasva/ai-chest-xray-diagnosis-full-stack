abfa12f00f6492d181f7db58207d4ba9176b8369
kanasva <94558160+kanasva@users.noreply.github.com>
2024-08-07 15:46:14 +1000

    edit footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,7 +1,21 @@
 export default function Footer() {
   return (
     <footer className="flex w-full justify-center bg-surface-container text-surface-on">
-      <div className="">Kan</div>
+      <div className="my-2 flex gap-8">
+        <span>
+          <a href="http://kanasva.me" target="_blank">
+            Kan
+          </a>
+        </span>
+        <span>
+          <a
+            href="https://github.com/kanasva/ai-chest-xray-diagnosis-full-stack"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </span>
+      </div>
     </footer>
   );
 }
