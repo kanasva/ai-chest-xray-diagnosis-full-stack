5d0067b0da050d89a8996177fb4ee39f6de51e18
kanasva <94558160+kanasva@users.noreply.github.com>
2024-08-11 00:43:00 +1000

    update facts

--- src/app/facts/page.tsx
+++ src/app/facts/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div className="flex w-full flex-1 items-start justify-center bg-surface">
       {/* content area max-w-1280 */}
-      <main className="flex max-w-[1280px] flex-1 flex-col items-center p-4 sm:p-6">
+      <main className="flex max-w-[1280px] flex-1 flex-col items-center px-4 pb-4 sm:px-6 sm:pb-4">
         <div className="mdx max-w-[800px]">
           <Content />
         </div>
