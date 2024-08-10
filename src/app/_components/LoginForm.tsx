504afbdd51a1143777a4c06da07a674e0855c7a6
kanasva <94558160+kanasva@users.noreply.github.com>
2024-08-11 01:25:00 +1000

    disclaimer

--- src/app/_components/LoginForm.tsx
+++ src/app/_components/LoginForm.tsx
@@ -24,7 +24,7 @@ export default function LoginForm() {
             {({ close }) => (
               <form className="flex flex-col items-center gap-6">
                 <div className="flex flex-col items-center gap-2">
-                  <h2 className="text-lg font-medium">Acknowledgement</h2>
+                  <h2 className="text-lg font-medium">Disclaimer</h2>
                   <p className="text-secondary">
                     This tool is not for medical use.
                   </p>
