61ebfb4e0d7702d5e4f00a415124ea5b7fbf25da
kanasva <94558160+kanasva@users.noreply.github.com>
2024-08-13 23:44:34 +1000

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Providers } from "./providers";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
           </div>
         </Providers>
       </body>
+      <GoogleAnalytics gaId="G-VP5CY9W3MQ" />
     </html>
   );
 }
