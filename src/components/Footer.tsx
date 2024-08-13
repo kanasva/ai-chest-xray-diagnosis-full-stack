78031d70e933772b36b9e0688766b73f7e4f8fe7
kanasva <94558160+kanasva@users.noreply.github.com>
2024-08-13 23:50:43 +1000

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,23 @@ import GitHubLogo from "./GitHubLogo";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full justify-end bg-surface-container px-4 text-surface-on sm:px-6">
-      <div className="my-2 flex gap-6">
-        <a href="http://kanasva.me" target="_blank">
-          Kan
-        </a>
-        <a
-          href="https://github.com/kanasva/ai-chest-xray-diagnosis-full-stack"
-          target="_blank"
-        >
-          <GitHubLogo
-            className="h-6 w-6 fill-surface-on"
-            title="GitHub respository"
-          />
-        </a>
-      </div>
-    </footer>
+    <div className="flex w-full justify-center bg-surface-container px-4 text-surface-on sm:px-6">
+      <footer className="flex w-full max-w-[1280px] justify-end">
+        <div className="my-2 flex gap-6">
+          <a href="http://kanasva.me" target="_blank">
+            Kan
+          </a>
+          <a
+            href="https://github.com/kanasva/ai-chest-xray-diagnosis-full-stack"
+            target="_blank"
+          >
+            <GitHubLogo
+              className="h-6 w-6 fill-surface-on"
+              title="GitHub respository"
+            />
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
